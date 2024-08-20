f13794c6f6f01ede87dea44f78e4c16b33802c30
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-08-20 22:00:01 +0530

    update

--- src/components/introduction/Intro.jsx
+++ src/components/introduction/Intro.jsx
@@ -17,27 +17,28 @@ function Intro() {
         <p className="intro-para">
           I am an aspiring web developer with entry-level professional skills in Web development. As self-motivated and hardworking developer seeking an opportunity to work in a challenging environment to prove my programming skills and utilize my knowledge of web dev technologies for the growth of the organization.
         </p>
-        <Link>
+        <Link to=''>
           <button className='hireme-btn'><PiHandbagSimpleFill className='bag-icon' /> Hire me</button>
         </Link>
       </div>
       <img className='bg-img' src={profImg} alt="Profile background Image" />
       <div className="bubbles-wrapper">
-        <span className="bubble" style='--bubble:1'></span>
-        <span className="bubble" style='--bubble:12'></span>
-        <span className="bubble" style='--bubble:16'></span>
-        <span className="bubble" style='--bubble:5'></span>
-        <span className="bubble" style='--bubble:20'></span>
-        <span className="bubble" style='--bubble:25'></span>
-        <span className="bubble" style='--bubble:13'></span>
-        <span className="bubble" style='--bubble:17'></span>
-        <span className="bubble" style='--bubble:11'></span>
-        <span className="bubble" style='--bubble:13'></span>
-        <span className="bubble" style='--bubble:19'></span>
-        <span className="bubble" style='--bubble:9'></span>
-        <span className="bubble" style='--bubble:21'></span>
-        <span className="bubble" style='--bubble:24'></span>
-        <span className="bubble" style='--bubble:15'></span>
+        <span className="bubble" style={{'--bubble':10}}></span>
+        <span className="bubble" style={{'--bubble':12}}></span>
+        <span className="bubble" style={{'--bubble':16}}></span>
+        <span className="bubble" style={{'--bubble':19}}></span>
+        <span className="bubble" style={{'--bubble':11}}></span>
+        <span className="bubble" style={{'--bubble':20}}></span>
+        <span className="bubble" style={{'--bubble':13}}></span>
+        <span className="bubble" style={{'--bubble':17}}></span>
+        <span className="bubble" style={{'--bubble':11}}></span>
+        <span className="bubble" style={{'--bubble':18}}></span>
+        <span className="bubble" style={{'--bubble':15}}></span>
+        <span className="bubble" style={{'--bubble':10}}></span>
+        <span className="bubble" style={{'--bubble':22}}></span>
+        <span className="bubble" style={{'--bubble':12}}></span>
+        <span className="bubble" style={{'--bubble':22}}></span>
+        <span className="bubble" style={{'--bubble':15}}></span>
       </div>
     </section>
   )
