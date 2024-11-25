01a5316c5e7fa5decfedaed6cdaf5130713e0416
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-11-25 20:28:57 +0530

    style: styles 'ContactMe' components

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -4,29 +4,29 @@ function ContactMe() {
   return (
     <section id="contact" className='contact'>
       <div className="form-wrapper">
-          <h3>Contact Me</h3>
+        <h3>Contact Me</h3>
         <form action="/" method="post">
           <div className="input-box">
-            <label htmlFor="name">Name:</label>
+            <label htmlFor="name">Name:</label> <br />
             <input id="name" name="" type="text" placeholder="Raghav Das" />
           </div>
           
           <div className="input-box">
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="email">Email:</label> <br />
             <input id="email" name="" type="email" placeholder="raghuvardas24@gmail.com" />
           </div>
           
           <div className="input-box">
-            <label htmlFor="mobile">Mobile No:</label>
+            <label htmlFor="mobile">Mobile No:</label> <br />
             <input id="mobile" name="" type="tel" placeholder="8551872404" />
           </div>
 
           <div className="input-box">
-            <label htmlFor="message">Message:</label>
+            <label htmlFor="message">Message:</label> <br />
             <textarea id="message" name="" type="text" ></textarea>
           </div>
 
-          <input type="submit" value="Submit" />
+          <input className='submit-btn' type="submit" value="Submit" />
         </form>
       </div>
     </section>
