cd33b0de02a0a3b214fc2bce9fd138fffd710e36
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-12-04 21:18:51 +0530

    updates

--- src/components/contact/ContactMe.jsx
+++ src/components/contact/ContactMe.jsx
@@ -1,31 +1,38 @@
 import contact from '/src/assets/images/contact-me.svg'
+import { FaUser, FaTelegramPlane } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import './ContactMe.css'
 
 function ContactMe() {
   return (
     <section id="contact" className='contact'>
-      <img src={contact} alt="Contact" />
-      <div className="form-wrapper">
-        <h3>Contact Me</h3>
-        <form action="/" method="post">
-          <div className="input-box">
-            <label htmlFor="name">Name:</label> <br />
-            <input id="name" name="" type="text" placeholder="Raghav Das" />
-          </div>
-          
-          <div className="input-box">
-            <label htmlFor="email">Email:</label> <br />
-            <input id="email" name="" type="email" placeholder="raghuvardas24@gmail.com" />
-          </div>
-          
-          <div className="input-box">
-            <label htmlFor="message">Message:</label> <br />
-            <textarea id="message" name="" type="text" ></textarea>
-          </div>
-
-          <input className='submit-btn' type="submit" value="Submit" />
-        </form>
+      <h3 className='contact-heading'>Contact Me</h3>
+      <div className="contact-img-wrapper">
+        <img src={contact} alt="Contact" />
       </div>
+      <form className="form-wrapper" action="/" method="post">
+        <div className="input-box">
+          <label htmlFor="name">Name:</label> <br />
+          <FaUser className='icon' />
+          <input id="name" name="" type="text" placeholder="Raghav Das" />
+        </div>
+        
+        <div className="input-box">
+          <label htmlFor="email">Email:</label> <br />
+          <MdEmail className='icon' />
+          <input id="email" name="" type="email" placeholder="raghuvardas24@gmail.com" />
+        </div>
+        
+        <div className="input-box">
+          <label htmlFor="message">Message:</label> <br />
+          <textarea id="message" name="" type="text" ></textarea>
+        </div>
+
+        <button className="submit-btn">
+          <FaTelegramPlane className='icon' />
+          Send Message
+        </button>
+      </form>
     </section>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaYoutube } from "react-icons/fa";
 import { FaXTwitter, FaCodepen } from "react-icons/fa6";
 
 import './Footer.css'
@@ -9,10 +9,11 @@ function Footer() {
     <footer>
       <h3>Vijay Jadhav</h3>
       <div className="footer-social-medias">
-        <a href='/'><FaGithub className='icon' /> </a>
-        <a href='/'><FaLinkedinIn className='icon' /> </a>
-        <a href='/'><FaCodepen className='icon' /> </a>
-        <a href='/'><FaXTwitter className='icon' /> </a>
+        <a href='https://github.com/vijay-jadhav1997' target="_blank"><FaGithub className='icon' /> </a>
+        <a href='https://www.linkedin.com/in/vijay-jadhav1997' target="_blank"><FaLinkedinIn className='icon' /> </a>
+        <a href='https://codepen.io/VIJAY-JADHAV-the-bashful/pens/public' target="_blank"><FaCodepen className='icon' /> </a>
+        <a href='https://x.com/VijayJadha93653' target="_blank"><FaXTwitter className='icon' /> </a>
+        <a href='https://www.youtube.com/@VijayJadhav-pw2jn' target="_blank"><FaYoutube className='icon' /> </a>
       </div>
       <span className="">© All right reserved</span>
     </footer>
