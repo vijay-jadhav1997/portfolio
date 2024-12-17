d1fc89c65d984d5acb2809e9db5b3faf7e318255
vijay-jadhav1997 <vijay05111997@gmail.com>
2024-12-17 21:17:46 +0530

    add skills components

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -1,18 +1,35 @@
 import './Header.css';
 import logo from '../../assets/logo.png';
 import contactImg from '../../assets/icons/contactImg.png';
-import { IoMenu } from "react-icons/io5";
+// import { IoMenu } from "react-icons/io5";
 import {Link} from 'react-scroll';
+import { useState } from 'react';
+
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  function menuHandler() {
+    // console.log("!!!...Jay Jay Ram Krushna Hari...!!!");
+    
+    if(!isMenuOpen) {
+      // console.log("!!!...Jay Vitthal Shree Vitthal...!!!");
+      setIsMenuOpen(true)
+    }
+    else {
+      // console.log("!!!...Shree Dnyanoba Mauli Tukaram...!!!");
+      setIsMenuOpen(false)
+    }
+  }
+  
   return (
     <header className='header'>
       <img className='logo' src={logo} alt="logo" />
-      <nav className="nav-menu">
-        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={700} className='nav-menu-item'>Home</Link>
-        {/* <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-30} duration={700} className='nav-menu-item'>About</Link> */}
-        <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-60} duration={700} className='nav-menu-item'>Projects</Link>
-        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={700} className='nav-menu-item'>Skills</Link>
+      <nav className={ isMenuOpen ? "nav-menu open" : "nav-menu"}>
+        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={700} className='nav-menu-item' onClick={menuHandler} >Home</Link>
+        {/* <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-30} duration={700} className='nav-menu-item' onClick={menuHandler} >About</Link> */}
+        <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-60} duration={700} className='nav-menu-item' onClick={menuHandler} >Projects</Link>
+        <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={700} className='nav-menu-item' onClick={menuHandler} >Skills</Link>
       </nav>
       <button className="contactBtn"
         onClick={() => {
@@ -22,7 +39,12 @@ const Header = () => {
         <img src={contactImg} alt="contact Image" className="contactBtn-img" />
         Contact Me
       </button>
-      <IoMenu className='humburger-menu-icon' />
+      <div className={ isMenuOpen ? "menu-wrapper open" : "menu-wrapper"} onClick={menuHandler}>
+        {/* <IoMenu className='humburger-menu-icon' /> */}
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </header>
   )
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,12 +1,30 @@
 import './Skills.css';
-import frontend from '../../assets/images/skills/frontend_tech.png';
-import jsLibraries from '../../assets/images/skills/react_redux.png';
-import backend from '../../assets/images/skills/backend.png';
-import github from '../../assets/images/skills/git_github.png';
-import restApi from '../../assets/images/skills/rest_api.png';
+// import frontend from '../../assets/images/skills/frontend_tech.png';
+// import jsLibraries from '../../assets/images/skills/react_redux.png';
+// import backend from '../../assets/images/skills/backend.png';
+// import github from '../../assets/images/skills/git_github.png';
+// import restApi from '../../assets/images/skills/rest_api.png';
+
+import javascript from '../../assets/images/js.png'
+import typescript from '../../assets/images/typescript.png'
+import reactjs from '../../assets/images/reactjs.png'
+import reduxjs from '../../assets/images/reduxjs.png'
+import html5 from '../../assets/images/html-5.png'
+import css3 from '../../assets/images/css-3.png'
+import tailwindcss from '../../assets/images/Tailwind.png'
+import bootstrap from '../../assets/images/bootstrap.png'
 
 import python from '../../assets/images/python.png'
-import javascript from '../../assets/images/js.png'
+import django from '../../assets/images/django2.png'
+
+// import restframework from '../../assets/images/restframework.png'
+import postgresql from '../../assets/images/postgresql.png'
+import redis from '../../assets/images/redis.png'
+import sql from '../../assets/images/redis.png'
+
+import git from '../../assets/images/git.png'
+import github from '../../assets/images/github.png'
+import npm from '../../assets/images/npm.png'
 
 const Skills = () => {
   return (
@@ -15,8 +33,22 @@ const Skills = () => {
       <p className='skills-description'>Passionate about coding and web development, I thrive on exploring new technologies and AI tools.  For me, coding is not just a career path – it is a lifelong pursuit fueled by curiosity, innovation, and a love for problem-solving.</p>
 
       <div className="skills-container">
+        <h3 className='skills-title'>Frontend Technologies</h3>
         <div className="frontend">
-          <h3>Frontend Technologies</h3>
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={html5} alt="HTML 5" />
+            </div>
+            <h4>HTML 5</h4>
+          </div>
+          
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={css3} alt="CSS 3" />
+            </div>
+            <h4>CSS 3</h4>
+          </div>
+        
           <div className="skill">
             <div className="img-wrapper">
               <img src={javascript} alt="Javascript" />
@@ -26,15 +58,119 @@ const Skills = () => {
 
           <div className="skill">
             <div className="img-wrapper">
-              <img src={python} alt="Typescript" />
+              <img src={typescript} alt="Typescript" />
             </div>
             <h4>Typescript</h4>
           </div>
           
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={reactjs} alt="React.js" />
+            </div>
+            <h4>React</h4>
+          </div>
+          
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={reduxjs} alt="Redux.js" />
+            </div>
+            <h4>Redux</h4>
+          </div>
+          
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={tailwindcss} alt="Tailwind CSS" />
+            </div>
+            <h4>Tailwind CSS</h4>
+          </div>
+          
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={bootstrap} alt="Bootstrap" />
+            </div>
+            <h4>Bootstrap</h4>
+          </div>
+          
         </div>
+
+        <h3 className='skills-title'>Backend Technologies</h3>
+        <div className="backend">
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={python} alt="Python" />
+            </div>
+            <h4>Python</h4>
+          </div>
+
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={django} alt="Django" />
+            </div>
+            <h4>Django</h4>
+          </div>
+          
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={django} alt="Django Rest Framework" />
+            </div>
+            <h4>Django Rest Framework</h4>
+          </div>
+          
+        </div>
+
+        <h3 className='skills-title'>Database</h3>
+        <div className="database">
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={sql} alt="SQL" />
+            </div>
+            <h4>SQL</h4>
+          </div>
+
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={postgresql} alt="Postgresql" />
+            </div>
+            <h4>Postgresql</h4>
+          </div>
+          
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={redis} alt="Redis" />
+            </div>
+            <h4>Redis</h4>
+          </div>
+          
+        </div>
+
+        <h3 className='skills-title' >Other</h3>
+        <div className="other" >
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={git} alt="Git" />
+            </div>
+            <h4>Git</h4>
+          </div>
+
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={github} alt="GitHub" />
+            </div>
+            <h4>GitHub</h4>
+          </div>
+          
+          <div className="skill">
+            <div className="img-wrapper">
+              <img src={npm} alt="npm" />
+            </div>
+            <h4>npm</h4>
+          </div>
+          
+        </div>
+
       </div>
       
-      <div className="skills-wrapper">
+      {/* <div className="skills-wrapper">
         <div className="skill-category">
           <img className="skill-category-img" src={frontend} alt="Image depicting logos of frontend technologies" />
           <div className="skill-category-details">
@@ -45,7 +181,7 @@ const Skills = () => {
               <li>JavaScript</li>
               <li>TypeScript</li>
               <li>Tailwind CSS</li>
-              {/* <li>Bootstrap</li> */}
+              <li>Bootstrap</li>
             </ul>
           </div>
         </div>
@@ -113,7 +249,7 @@ const Skills = () => {
           </div>
         </div>
         
-      </div>
+      </div> */}
 
     </section>
   )
